debbcdcb6fae53d245d8ad42d13b6932c806cc04
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 13:29:06 +0530

    third commit

---
+++ src/main/java/com/anjaniy/redditclonebackend/models/NotificationEmail.java
@@ -0,0 +1,15 @@
+package com.anjaniy.redditclonebackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationEmail {
+
+    private String subject;
+    private String recipient;
+    private String body;
+}
