a8c1bf7bec3ad49c3780e58854eac0d663fcf55e
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 14:29:41 +0530

    forth commit

--- src/main/java/com/anjaniy/redditclonebackend/services/AuthService.java
+++ src/main/java/com/anjaniy/redditclonebackend/services/AuthService.java
@@ -2,7 +2,9 @@ package com.anjaniy.redditclonebackend.services;
 
 import com.anjaniy.redditclonebackend.dto.RegisterRequest;
 import com.anjaniy.redditclonebackend.models.User;
+import com.anjaniy.redditclonebackend.models.VerificationToken;
 import com.anjaniy.redditclonebackend.repositories.UserRepo;
+import com.anjaniy.redditclonebackend.repositories.VerificationTokenRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -21,8 +24,11 @@ public class AuthService {
 //    @Autowired
     private final UserRepo userRepo;
 
+//    @Autowired
+    private final VerificationTokenRepo verificationTokenRepo;
+
     @Transactional
-    public void signup(RegisterRequest registerRequest){
+    public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setEmail(registerRequest.getEmail());
         user.setUsername(registerRequest.getUsername());
@@ -31,5 +37,17 @@ public class AuthService {
         user.setEnabled(false);
 
         userRepo.save(user);
+
+        String vToken = generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String vToken = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(vToken);
+        verificationToken.setUser(user);
+
+        verificationTokenRepo.save(verificationToken);
+        return vToken;
     }
 }
