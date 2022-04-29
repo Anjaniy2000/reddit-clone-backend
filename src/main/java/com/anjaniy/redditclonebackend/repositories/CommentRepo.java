ae59410b0882f5a3ab5a8c7f69168c9dc10465c1
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 13:24:39 +0530

    third commit

---
+++ src/main/java/com/anjaniy/redditclonebackend/repositories/CommentRepo.java
@@ -0,0 +1,9 @@
+package com.anjaniy.redditclonebackend.repositories;
+
+import com.anjaniy.redditclonebackend.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepo extends JpaRepository<Comment, Long> {
+}
