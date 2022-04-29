63165daf0900e841b4982679545b57661a7899c9
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 13:21:21 +0530

    third commit

--- src/main/java/com/anjaniy/redditclonebackend/repositories/SubredditRepo.java
+++ src/main/java/com/anjaniy/redditclonebackend/repositories/SubredditRepo.java
@@ -1,9 +1,9 @@
 package com.anjaniy.redditclonebackend.repositories;
 
-import com.anjaniy.redditclonebackend.models.Post;
+import com.anjaniy.redditclonebackend.models.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepo extends JpaRepository<Post, Long> {
+public interface SubredditRepo extends JpaRepository<Subreddit, Long> {
 }
