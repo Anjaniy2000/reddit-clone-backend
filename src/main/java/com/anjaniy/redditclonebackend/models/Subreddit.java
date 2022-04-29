2e5b20aa6867c2be809eb39df0efcae2c810b450
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 12:49:43 +0530

    second commit

--- src/main/java/com/anjaniy/redditclonebackend/models/Subreddit.java
+++ src/main/java/com/anjaniy/redditclonebackend/models/Subreddit.java
@@ -1,4 +1,37 @@
 package com.anjaniy.redditclonebackend.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Community name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Post> posts;
+
+    private Instant createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
