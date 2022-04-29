02e5f820290b641ec3a74d6648bc8450a603ccc0
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 13:14:18 +0530

    second commit

---
+++ src/main/java/com/anjaniy/redditclonebackend/models/Comment.java
@@ -0,0 +1,35 @@
+package com.anjaniy.redditclonebackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    private Instant createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
