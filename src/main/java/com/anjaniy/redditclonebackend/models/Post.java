0a877311cbfbb28f0b1d000bd6b0fc4e613acfc2
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 12:41:33 +0530

    second commit

--- src/main/java/com/anjaniy/redditclonebackend/models/Post.java
+++ src/main/java/com/anjaniy/redditclonebackend/models/Post.java
@@ -1,10 +1,9 @@
 package com.anjaniy.redditclonebackend.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
+import lombok.*;
+import org.springframework.lang.Nullable;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 @Data
@@ -14,12 +13,28 @@ import java.time.Instant;
 @AllArgsConstructor
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
+
+    @NotBlank(message = "Post name can not be empty or Null")
     private String postName;
+
+    @Nullable
     private String url;
+
+    @Lob
     private String description;
+
     private Long voteCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
     private User user;
+
     private Instant createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
     private Subreddit subreddit;
 }
