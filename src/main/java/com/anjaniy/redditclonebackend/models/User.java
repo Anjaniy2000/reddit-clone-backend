587b340f86f9a628aa378c97d9f47dbe2e7e6991
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-04-29 13:00:11 +0530

    second commit

--- src/main/java/com/anjaniy/redditclonebackend/models/User.java
+++ src/main/java/com/anjaniy/redditclonebackend/models/User.java
@@ -11,11 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.time.Instant;
 
 @Data
 @Entity
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
@@ -31,7 +31,7 @@ public class User {
     private String password;
 
     @Email
-    @NotBlank(message = "Email is required")
+    @NotEmpty(message = "Email is required")
     private String email;
 
     private Instant createDate;
---
+++ src/main/java/com/anjaniy/redditclonebackend/models/VerificationToken.java
@@ -0,0 +1,27 @@
+package com.anjaniy.redditclonebackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@Entity
+@Table(name = "token")
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private Instant expiryDate;
+}
