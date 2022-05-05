40ad615dae2f26a53f051efdd498682984373838
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-05-05 23:27:02 +0530

    tenth commit

---
+++ src/main/java/com/anjaniy/redditclonebackend/mappers/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.anjaniy.redditclonebackend.mappers;
+
+import com.anjaniy.redditclonebackend.dto.CommentDto;
+import com.anjaniy.redditclonebackend.models.Comment;
+import com.anjaniy.redditclonebackend.models.Post;
+import com.anjaniy.redditclonebackend.models.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    Comment map(CommentDto commentDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentDto mapToDto(Comment comment);
+}
--- src/main/java/com/anjaniy/redditclonebackend/services/CommentService.java
+++ src/main/java/com/anjaniy/redditclonebackend/services/CommentService.java
@@ -1,6 +1,10 @@
 package com.anjaniy.redditclonebackend.services;
 
 import com.anjaniy.redditclonebackend.dto.CommentDto;
+import com.anjaniy.redditclonebackend.exceptions.PostNotFoundException;
+import com.anjaniy.redditclonebackend.models.Post;
+import com.anjaniy.redditclonebackend.repositories.PostRepo;
+import com.anjaniy.redditclonebackend.repositories.UserRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +12,14 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CommentService {
 
+    private final PostRepo postRepo;
+    private final UserRepo userRepo;
+    private final AuthService authService;
+
     public void save(CommentDto commentDto){
+        Post post = postRepo.findById(commentDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentDto.getPostId().toString()));
+
 
     }
 }
