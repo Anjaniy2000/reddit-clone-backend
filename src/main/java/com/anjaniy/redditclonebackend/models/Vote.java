5e06c64d730f760658af318f4b644a48dda31e21
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-05-07 15:20:53 +0530

    eleventh

--- src/main/java/com/anjaniy/redditclonebackend/models/Vote.java
+++ src/main/java/com/anjaniy/redditclonebackend/models/Vote.java
@@ -8,25 +8,24 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Vote {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @GeneratedValue(strategy = IDENTITY)
+    private Long voteId;
     private VoteType voteType;
-
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "postId", referencedColumnName = "postId")
     private Post post;
-
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "userId", referencedColumnName = "userId")
     private User user;
 }
