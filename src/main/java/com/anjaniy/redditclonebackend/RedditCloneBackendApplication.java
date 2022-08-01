847367b57efe26609b8efae51bc6a65a3f5cec12
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-08-01 18:25:36 +0530

    final refactoring.

--- src/main/java/com/anjaniy/redditclonebackend/RedditCloneBackendApplication.java
+++ src/main/java/com/anjaniy/redditclonebackend/RedditCloneBackendApplication.java
@@ -1,14 +1,20 @@
 package com.anjaniy.redditclonebackend;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@OpenAPIDefinition(info = @Info(title = "Reddit Clone API", version = "1.0", description = "This API Is For Reddit Clone Frontend Application."))
+@OpenAPIDefinition(info = @Info(title = "Reddit Clone API",
+		version = "1.0",
+		description = "This API Is For Reddit Clone Frontend Application.",
+		license = @License(name = "MIT License"),
+		contact = @Contact(name = "Anjaniy Salekar", email = "anjaniy01salekar@gmail.com")))
 public class RedditCloneBackendApplication {
 
 	public static void main(String[] args) {
