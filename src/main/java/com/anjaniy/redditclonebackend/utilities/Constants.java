d0a521295d354f1e62d70e686006412313db577f
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-08-01 13:41:44 +0530

    final

--- src/main/java/com/anjaniy/redditclonebackend/utilities/Constants.java
+++ src/main/java/com/anjaniy/redditclonebackend/utilities/Constants.java
@@ -4,5 +4,5 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Constants {
-    public static final String ACTIVATION_EMAIL = "http://localhost:8080/api/auth/accountVerification";
+    public static final String ACTIVATION_EMAIL = "https://reddit-clone-sb.herokuapp.com/api/auth/accountVerification";
 }
